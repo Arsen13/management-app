cb2e62030abb6f3d2b4900107fdad4606d945b18
Arsen Kohut <arsenkogut2003@gmail.com>
2025-05-23 11:17:46 +0300

    Add TypeORM to project and create user entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DB_URL'),
+        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}

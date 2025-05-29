160ca45d7eb8fa1fd76b3c2c3101d25e711ef157
Arsen Kohut <arsenkogut2003@gmail.com>
2025-05-29 20:23:26 +0300

    Implement logout functionality

--- frontend/src/components/navbar/LogoutBtn.tsx
+++ frontend/src/components/navbar/LogoutBtn.tsx
@@ -1,7 +1,16 @@
 import { CiLogout } from "react-icons/ci";
+import { removeToken } from "../../lib/localStorage.helper";
+import { useNavigate } from "react-router-dom";
 
 export default function LogoutBtn() {
+  const navigate = useNavigate();
   return (
-    <CiLogout className="w-8 h-8 hover:text-red-500 text-white duration-300 cursor-pointer" />
+    <CiLogout
+      onClick={() => {
+        removeToken("token");
+        navigate("/login");
+      }}
+      className="w-8 h-8 hover:text-red-500 text-white duration-300 cursor-pointer"
+    />
   );
 }

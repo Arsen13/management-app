d95455982b15130838b326f4ed663c3e21aacebb
Arsen Kohut <arsenkogut2003@gmail.com>
2025-05-30 20:45:42 +0300

    fix typo

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,6 +4,6 @@ import { getItem } from "../lib/localStorage.helper";
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:4200/api",
   headers: {
-    Authorization: "Bearer" + getItem("token") || "",
+    Authorization: `Bearer ${getItem("token")}` || "",
   },
 });
